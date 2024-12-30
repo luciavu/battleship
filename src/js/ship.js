66733c9b7d7932a4d602676fdc76bb9108cd55b4
Lucia Vu <luciavu6@outlook.com>
2024-12-31 01:22:15 +1000

    Add ship class and tests

---
+++ src/js/ship.js
@@ -0,0 +1,18 @@
+export default class Ship {
+  constructor(length) {
+    this.length = length;
+    this.hits = 0;
+    this.hasSunk = false;
+  }
+
+  hit() {
+    if (this.hits < this.length) {
+      this.hits++;
+    }
+  }
+
+  isSunk() {
+    this.hasSunk = this.length === this.hits;
+    return this.hasSunk;
+  }
+}
---
+++ src/js/ship.test.js
@@ -0,0 +1,40 @@
+import Ship from './ship';
+
+describe('Ship', () => {
+  let ship;
+  const length = 3;
+
+  beforeEach(() => {
+    ship = new Ship(length);
+  });
+
+  test('Ship object created with correct given length', () => {
+    expect(ship.length).toBe(length);
+  });
+
+  test('Ship object hits updated correctly', () => {
+    ship.hit();
+    expect(ship.hits).toBe(1);
+  });
+
+  test('Ship hits should not exceed length', () => {
+    for (let i = 0; i <= length; i++) {
+      ship.hit();
+    }
+    expect(ship.hits).toBe(length);
+  });
+
+  test('isSunk returns false when ship has not sunk', () => {
+    for (let i = 0; i < length - 1; i++) {
+      ship.hit();
+    }
+    expect(ship.isSunk()).toBeFalsy();
+  });
+
+  test('isSunk returns true when ship has sunk', () => {
+    for (let i = 0; i < length; i++) {
+      ship.hit();
+    }
+    expect(ship.isSunk()).toBeTruthy();
+  });
+});
